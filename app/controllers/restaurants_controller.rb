ef91033a801dba5393a79dbbae2c30056cafc00b
Nathan Babalogbon <nathanbabalogbon@gmail.com>
2023-05-31 14:32:32 +0100

    'new' form added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,17 +7,17 @@ class RestaurantsController < ApplicationController
   #   @restaurant = Restaurant.find_by_id(params[:id])
   # end
 
-  # def new
-  #   @restaurant = Restaurant.new
-  # end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-  # def create
-  #   @restaurant = Restaurant.new(params[:restaurant])
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
 
-  #   if @restaurant.save
-  #     redirect_to restaurants_path
-  #   else
-  #     render :new
-  #   end
-  # end
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
 end
