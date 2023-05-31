230de489a29b0fa36c4ed7a4deb8f7384915d670
Nathan Babalogbon <nathanbabalogbon@gmail.com>
2023-05-31 14:12:38 +0100

    restaurant index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # def show
+  #   @restaurant = Restaurant.find_by_id(params[:id])
+  # end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def create
+  #   @restaurant = Restaurant.new(params[:restaurant])
+
+  #   if @restaurant.save
+  #     redirect_to restaurants_path
+  #   else
+  #     render :new
+  #   end
+  # end
 end
